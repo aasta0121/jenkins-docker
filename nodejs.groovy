7e611ad1bcee5c2c4a9be9bfc30fb4d91bfc3c4c
aasta0121 <65644589+aasta0121@users.noreply.github.com>
2023-11-13 15:48:21 +0800

    Update nodejs.groovy

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/aasta0121/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('zih.sin.0121@gmail.com')
         }
     }
     triggers {
